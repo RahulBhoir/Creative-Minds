bcbf4bc865394d452b1896b3f460dd522012104f
Rahul <rahulpbhoir@outlook.com>
2020-11-08 15:50:10 +0530

    modify

--- creative_minds/creative_minds/settings.py
+++ creative_minds/creative_minds/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'creative_minds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR,
+                 os.path.join(BASE_DIR, 'user/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
