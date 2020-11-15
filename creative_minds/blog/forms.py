ee8048879b05ec4d964550f5ac7248ac6f41ccbf
Rahul <rahulpbhoir@outlook.com>
2020-11-15 12:38:08 +0530

    medium style editor is working properly

--- creative_minds/blog/forms.py
+++ creative_minds/blog/forms.py
@@ -12,17 +12,17 @@ class UserRegistrationForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class PostCreationForm(forms.Form):
-    title = forms.CharField()
-    text = forms.CharField(widget=forms.Textarea)
-    #
-    # class Meta:
-    #     model = Post
-    #     fields = ['title', 'text']
-        # widgets = {
-        #     'title': forms.TextInput(attrs={'class': 'textinputclass'}),
-        #     'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'})
-        # }
+class PostCreationForm(forms.ModelForm):
+    # title = forms.CharField()
+    # text = forms.CharField()  # widget=forms.Textarea
+
+    class Meta:
+        model = Post
+        fields = ['title', 'text']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'})
+        }
 
 
 class PostUpdateForm(forms.ModelForm):
